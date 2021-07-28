3f102a4a787fb02f189b84ca1c1279ca6505fc34
Juliana Meneguete <jumeneguete@gmail.com>
2021-07-27 21:27:33 -0300

    Initial setup

---
+++ build/static/js/main.a1aa2dd9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprepoprovas=this.webpackJsonprepoprovas||[]).push([[0],{3:function(e,o){},5:function(e,o,r){"use strict";r.r(o);var n=r(2),s=r.n(n),t=r(3),p=r.n(t),c=r(4);s.a.render(Object(c.jsx)(p.a,{}),document.querySelector("root"))}},[[5,1,2]]]);
+//# sourceMappingURL=main.a1aa2dd9.chunk.js.map
